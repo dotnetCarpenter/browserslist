407e35fd248d2cb7d0f0f974d4de76ca3d06fe37
dotnetCarpenter <jon.ronnenberg@gmail.com>
2019-01-09 21:30:48 +0100

    giving up on regex parsing

---
+++ test/parse.test.js
@@ -0,0 +1,50 @@
+var parse = require('../parse')
+
+it('can parse single browser query', () => {
+  var expected = [{ type: parse.QUERY_OR, queryString: 'ie 6' }]
+  var actual = parse('ie 6')
+
+  expect(actual).toEqual(expected)
+})
+
+it('can parse single version query', () => {
+  var expected = [
+    {
+      ype: parse.QUERY_OR,
+      queryString: 'last 3 Chrome major versions'
+    }
+  ]
+  var actual = parse('last 3 Chrome major versions')
+
+  expect(actual).toEqual(expected)
+})
+
+it('can parse two OR queries', () => {
+  var expected = [
+    { type: parse.QUERY_OR, queryString: 'defaults' },
+    { type: parse.QUERY_OR, queryString: 'ie 6' }
+  ]
+  var actual = parse('defaults or ie 6')
+
+  expect(actual).toEqual(expected)
+})
+
+it('can parse two "," queries', () => {
+  var expected = [
+    { type: parse.QUERY_OR, queryString: 'defaults' },
+    { type: parse.QUERY_OR, queryString: 'ie 6' }
+  ]
+  var actual = parse('defaults, ie 6')
+
+  expect(actual).toEqual(expected)
+})
+
+it('can parse two AND queries', () => {
+  var expected = [
+    { type: parse.QUERY_OR, queryString: 'defaults' },
+    { type: parse.QUERY_AND, queryString: 'ie 6' }
+  ]
+  var actual = parse('defaults and ie 6')
+
+  expect(actual).toEqual(expected)
+})
